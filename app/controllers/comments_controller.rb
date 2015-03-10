9422a5c9a4851d35a507d11c926295e3b920e2b6
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-10 18:24:03 -0400

    Add edit functionality to comment body.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = current_user.comments.new(comment_params)
+    @comment = current_user.comments.new(new_comment_params)
     if @comment.save
       redirect_to post_url(@comment.post_id)
     else
@@ -14,8 +14,27 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(edit_comment_params)
+      redirect_to post_url(@comment.post_id)
+    else
+      flash[:errors] = @comment.errors.full_messages
+      redirect_to edit_comment_url(@comment)
+    end
+  end
+
   private
-  def comment_params
+  def new_comment_params
     params.require(:comment).permit(:content, :parent_id, :post_id)
+  end
+
+  def edit_comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :shows, only: [:show] do
