15f7129cb966256a8bb8cb8a146d765f1a9018c4
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-10 09:59:02 -0400

    Initialize repo for Backbone functionality.

---
+++ app/assets/javascripts/project_proposal.js
@@ -0,0 +1,13 @@
+window.MediaPassport = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  MediaPassport.initialize();
+});
