88c4a0ee1432810fc6b8446db9dfe447feaf534f
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-19 16:52:07 -0400

    Fix more Action Mailer env stuff in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'media-passport.heroku.com' }
+
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['sendgrid_username'],
