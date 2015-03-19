0249a67d04aa593fc38b919558af8194ec29bf92
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-19 17:10:39 -0400

    Change thumbnail image size to 50x50.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_attached_file :avatar,
-    styles: { thumb: "100x100>", medium: "300x300>" },
+    styles: { thumb: "50x50>", medium: "300x300>" },
     default_url: "missing.:style.gif"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
