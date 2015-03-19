bd088f9b2c73b7afcafad33d062ed1c6df3d0e31
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-19 10:41:22 -0400

    Add figaro and production actionmailer support via SendGrid.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'pg'
 
 gem 'pg_search'
 gem 'kaminari'
+gem 'figaro'
 
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['sendgrid_username'],
+  :password => ENV['sendgrid_password'],
+  :domain => 'media-passport.heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
