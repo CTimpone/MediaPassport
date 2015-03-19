65e8d03c696b09797c89624196beb2ea366afea7
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-19 17:05:32 -0400

    Fix s3 host for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
           :bucket => ENV["S3_BUCKET"],
           :access_key_id => ENV["S3_ACCESS_KEY_ID"],
           :secret_access_key => ENV["S3_SECRET_ACCESS_KEY"],
-          :s3_host_name => "s3.amazonaws.com"
+          :s3_host_name => "s3-us-west-2.amazonaws.com"
         }
       }
 end
